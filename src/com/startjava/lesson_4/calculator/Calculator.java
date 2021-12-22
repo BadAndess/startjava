fdc7bf7467ddcc4c6557d8fd6223fe9dc3d5b232
BadAndess <69672826+BadAndess@users.noreply.github.com>
2021-12-22 20:01:21 +0300

    Add files via upload

--- src/com/startjava/lesson_4/calculator/Calculator.java
+++ src/com/startjava/lesson_4/calculator/Calculator.java
@@ -2,48 +2,28 @@ package com.startjava.lesson_4.calculator;
 
 public class Calculator {
 
-    private int number1;
-    private int number2;
-    private char sign;
-
-    public void setNumber1(int number1) {
-        this.number1 = number1;
-    }
-
-    public void setNumber2(int number2) {
-        this.number2 = number2;
-    }
-
-    public void setSign(char sign) {
-        this.sign = sign;
-    }
+    int  a = Integer.parseInt(str[0]);
+    int  b = Integer.parseInt(str[2]);
+    int result;
 
     public void calc() {
-        switch (sign) {
-            case '+':
-                System.out.println(number1 + number2);
+        switch (str[1]) {
+            case "+":
+                System.out.println(Math.abs(a + b));
                 break;
-            case '-':
-                System.out.println(number1 - number2);
+            case "-":
+                System.out.println(Math.abs(a - b));
                 break;
-            case '*':
-                System.out.println(number1 * number2);
+            case "/":
+                System.out.println(Math.abs(a / b));
                 break;
-            case '/':
-                System.out.println(number1 / number2);
+            case "*":
+                System.out.println(Math.abs(a * b));
                 break;
-            case '^':
-                int result = 1;
-                for (int i = 1; i <= number2; i++) {
-                    result *= number1;
-                }
-                System.out.println(result);
+            case "^":
+                System.out.println(Math.pow(a, b));
                 break;
-            case '%':
-                System.out.println(number1 % number2);
-                break;
-            default:
-                System.out.println("Incorrect sign ");
         }
     }
+
 }
